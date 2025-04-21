c70b63bcbe173aa07fc3d611eacd9a9c0b856fb1
Andrei Faccin <andrei.faccin.ti@gmail.com>
2025-04-21 18:46:20 -0300

    Modulo 2 curso Otavio Miranda

--- cursoJS-OtavioMiranda/Tabela IMC/assets/js/main.js
+++ cursoJS-OtavioMiranda/Tabela IMC/assets/js/main.js
@@ -1,3 +1,21 @@
-function calculaIMC (){
-    
+// Capturar o evento de submit do formulário
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault(); // 
+    const inputPeso = e.target.querySelector('#peso');
+});
+
+function criaP () {
+    const p = document.createElement('p'); // cria um paragrafo
+    return p;
+
+}
+
+function setResultado (msg){
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    //resultado.innerHTML = `<p>${msg}</p>`;
+
+    const p = criaP();
 }
